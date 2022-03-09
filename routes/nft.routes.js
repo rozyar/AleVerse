7c91c0d6d0af4b27881e96116118c9d2351e83ef
Rayzor Rayzor <rayzor@arboleya.me>
2022-03-09 17:59:46 -0300

    route /my-nfts

--- routes/nft.routes.js
+++ routes/nft.routes.js
@@ -6,25 +6,52 @@ const attachCurrentUser = require("../middlewares/attachCurrentUser");
 const UserModel = require("../models/User.model");
 const NftModel = require("../models/Nft.model");
 
+router.post(
+  "/create-nfts",
+  isAuthenticated,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.currentUser;
 
-router.post("/create-nfts", isAuthenticated, attachCurrentUser, async (req, res) => {
-    try{
+      const createNft = await NftModel.create({
+        ...req.body,
+        author: loggedInUser._id,
+      });
 
-        const loggedInUser = req.currentUser;
+      await NftModel.findOneAndUpdate(
+        { _id: loggedInUser._id },
+        {
+          $push: { nfts: createNft._id },
+        },
+        { new: true, runValidators: true }
+      );
 
-        const createNft =  await NftModel.create({...req.body, author: loggedInUser._id})
-
-     await NftModel.findOneAndUpdate({ _id: loggedInUser._id},
-            {
-                $push:{nfts: createNft._id},
-            },{new: true, runValidators: true})
-
-
-        return res.status(201).json(createNft);
-    }catch(error){
-        console.log(error);
-        return res.status(500).json({msg: JSON.stringify(error)})
+      return res.status(201).json(createNft);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ msg: JSON.stringify(error) });
     }
-})
+  }
+);
+
+router.get("/my-nfts", isAuthenticated, attachCurrentUser, async (req, res) => {
+  try {
+    const loggedInUser = req.currentUser;
+
+    const userNfts = await NftModel.find(
+      { author: loggedInUser._id },
+      { author: 0 }
+    );
+    return res.status(200).json(userNfts);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+});
+
+
 
 module.exports = router;
+
+
